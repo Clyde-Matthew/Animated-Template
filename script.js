4d11db0698e314801867ec44a1009ae173517f06
Clyde-Matthew <clyde.matthew1@gmail.com>
2022-03-25 08:36:05 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+
+  AOS.init({
+      delay:200,
+      duration:1500,
+      once:false,//whether animation should happen only once while scrolling
+      mirror:false,//whether elements should animate while scrolling
+  });
