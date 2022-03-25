e0514838418d42898c078d8c0675bd649306e777
Clyde-Matthew <clyde.matthew1@gmail.com>
2022-03-25 09:15:13 +0200

    third commit

--- script.js
+++
@@ -1,7 +0,0 @@
-
-  AOS.init({
-      delay:200,
-      duration:1500,
-      once:false,//whether animation should happen only once while scrolling
-      mirror:false,//whether elements should animate while scrolling
-  });
